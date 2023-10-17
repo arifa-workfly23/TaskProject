cc213f7631fb9a2810319f788dae180b0d650a1a
arifa-workfly23 <130303941+arifa-workfly23@users.noreply.github.com>
2023-10-17 08:36:29 +0530

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -3,7 +3,7 @@ import json
 import logging
 # import related models here
 from requests.auth import HTTPBasicAuth
-from . import models
+from .models import CarDealer, DealerReview  
 from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 import Features, SentimentOptions
