b3d37902870c1edc91f43fb5756833b15d1d58da
arifa-workfly23 <130303941+arifa-workfly23@users.noreply.github.com>
2023-10-20 16:21:10 +0530

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -5,13 +5,22 @@ from .models import *
 # Register your models here.
 class CarModelInline(admin.StackedInline):
     model = CarModel
-    extra = 5
+    extra = 2
+
+
+# CarModelAdmin class 
+class CarModelAdmin(admin.ModelAdmin):
+ list_display = ('name', 'cartype', 'year') 
+ list_filter = ['year'] 
+ search_fields = ['name', 'type'] 
+    
 
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
+    list_display = ('name', 'description') 
 
 admin.site.register(CarMake, CarMakeAdmin)
-admin.site.register(CarModel)
+admin.site.register(CarModel, CarModelAdmin)
 
 # CarModelInline class
 
